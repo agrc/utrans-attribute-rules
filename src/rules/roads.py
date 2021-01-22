d8e0f41d83db8cbe65c4dc7e33f2ffc40cce8ba2
Greg Bunce <gbunce@utah.gov>
2021-01-22 15:25:35 -0700

    fix upper calculations
    and set triggers to update; also rename ar rules to reflect the associated field name

--- src/rules/roads.py
+++ src/rules/roads.py
@@ -16,7 +16,6 @@ FOLDER = 'roads'
 
 fullname_calculation = Calculation('Full Name', 'FULLNAME', load_rule_for(FOLDER, 'fullNameCalculation'))
 fullname_calculation.triggers = [config.triggers.insert, config.triggers.update]
-fullname_calculation.editable = config.editable.no
 
 fromaddress_left_calculation = Calculation('From Address Left', 'FROMADDR_L', common.set_null_as_zero('FROMADDR_L'))
 fromaddress_left_calculation.triggers = [config.triggers.insert, config.triggers.update]
@@ -24,24 +23,17 @@ fromaddress_left_calculation.triggers = [config.triggers.insert, config.triggers
 fromaddress_right_calculation = Calculation('From Address Right', 'FROMADDR_R', common.set_null_as_zero('FROMADDR_R'))
 fromaddress_right_calculation.triggers = [config.triggers.insert, config.triggers.update]
 
-toaddress_left_calculation = Calculation('To Address Left', 'TOADDR_L', common.set_null_as_zero('TOADDR_L'))
+toaddress_left_calculation = Calculation('From Address Left', 'TOADDR_L', common.set_null_as_zero('TOADDR_L'))
 toaddress_left_calculation.triggers = [config.triggers.insert, config.triggers.update]
 
-toaddress_right_calculation = Calculation('To Address Right', 'TOADDR_R', common.set_null_as_zero('TOADDR_R'))
+toaddress_right_calculation = Calculation('From Address Right', 'TOADDR_R', common.set_null_as_zero('TOADDR_R'))
 toaddress_right_calculation.triggers = [config.triggers.insert, config.triggers.update]
 
-name_calculation = Calculation('Name', 'NAME', 'Upper($feature.NAME)')
-name_calculation.triggers = [config.triggers.insert, config.triggers.update]
-
-alias_name_calculation = Calculation('Alias Name', 'A1_NAME', 'Upper($feature.A1_NAME)')
-alias_name_calculation.triggers = [config.triggers.insert, config.triggers.update]
-
-alias_alternate_name_calculation = Calculation('Alternate Alias Name', 'A2_NAME', 'Upper($feature.A2_NAME)')
-alias_alternate_name_calculation.triggers = [config.triggers.insert, config.triggers.update]
+name_calculation = Calculation('Name', 'NAME', 'Upper(\'NAME\')')
+alias_name_calculation = Calculation('Alias Name', 'A1_NAME', 'Upper(\'A1_NAME\')')
+alias_alternate_name_calculation = Calculation('Alternate Alias Name', 'A2_NAME', 'Upper(\'A2_NAME\')')
 
 predir_domain_constraint = Constraint('Prefix direction', 'PREDIR', common.constrain_to_domain('PREDIR', allow_null=True, domain='PreDirDomainName'))
-predir_domain_constraint.triggers = [config.triggers.insert, config.triggers.update]
-
 
 RULES = [
     fullname_calculation,
